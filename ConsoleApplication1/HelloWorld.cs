42be21dca85910a6ae522bce21728b52dca32987
Nics Projects <nicsprojects25@gmail.com>
2021-11-20 01:47:48 +0700

    it was born

---
+++ ConsoleApplication1/HelloWorld.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCSharp
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            #region
+            //Pertama
+            /*Console.Write("Hello World");
+            /Console.Read();*/
+
+            //Kedua
+            /*Console.WriteLine("Hello Nama Saya C SHARP");
+            Console.WriteLine("Saya Kuliah di STT-PLN");
+            Console.WriteLine("Jurusan Teknik Informatika");
+            Console.Read();*/
+
+            Console.WriteLine("  -----------------------------------------------------");
+            Console.WriteLine("  | FlatCode.net                                      |");
+            Console.WriteLine("  | Jl.Ayahanda No 24 A                               |");
+            Console.WriteLine("  | Telp. 082275192293                                |");
+            Console.WriteLine("  | PLG000001                                         |");
+            Console.WriteLine("  | Afiifah Ramadhani                                 |");
+            Console.WriteLine("  | Jl. Pelita II                                     |");
+            Console.WriteLine("  | ---------------------------------------------------");
+            Console.WriteLine("  | Nama                       Qty     Harga    Total |");
+            Console.WriteLine("  | ---------------------------------------------------");
+            Console.WriteLine("  | Nextar Cokelat              5      15,000   75,000|");
+            Console.WriteLine("  | Biskuit cokelat lezat                             |");
+            Console.WriteLine("  | Pocari Sweat                2       7,600   15,200|");
+            Console.WriteLine("  | Minuman ion                                       |");
+            Console.WriteLine("  | Waffer Tango                3      13,300   39,000|");
+            Console.WriteLine("  | Biskuit rasa vanilla                              |");
+            Console.WriteLine("  | ---------------------------------------------------");
+            Console.WriteLine("  | Total -- 3 -- Item(s).     Rp.130,100             |");
+            Console.WriteLine("  | Tunai                      Rp.150,000             |");
+            Console.WriteLine("  | Kembali                    Rp.19,900              |");
+            Console.WriteLine("  | ---------------------------------------------------");
+            Console.WriteLine("  | PLJ1241636212-001                                 |");
+            Console.WriteLine("  | 10/02/2017 14.35 Opr:Master                       |");
+            Console.WriteLine("  | Periksa barang sebelum dibeli                     |");
+            Console.WriteLine("  | Barang yang sudah dibeli                          |");
+            Console.WriteLine("  | Tidak bisa ditukar atau dikembalikan !            |");
+            Console.WriteLine("  -----------------------------------------------------");
+            Console.Read();
+            #endregion
+            
+        }
+    }
+}
